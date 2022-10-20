b53840e2cf2c1eff933e9a39393dc745548e0e39
marshalljunior007 <marshalljunior007@gmail.com>
2022-10-19 22:14:18 -0700

    add

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,7 +5,8 @@
  * @n: the starting number
  */
 void print_to_98(int n)
-{ if (n >= 98)
+{
+       	if (n >= 98)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,11 @@ int print_last_digit(int n)
 	int ld = n % 10;
 
 	if (ld < 0)
+	{
 		ld *= -1;
 
 	_putchar(ld + '0');
+	}
 
 	return (0);
 }
