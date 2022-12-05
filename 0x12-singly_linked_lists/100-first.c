954e8c8d965cae7d7bb489bc4860a87d0adf517f
marshalljunior007 <marshalljunior007@gmail.com>
2022-12-04 20:41:15 -0800

    the works

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+nclude <stdio.h>
+
+void first(void) __attribute__ ((constructor));
+
+/**
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
